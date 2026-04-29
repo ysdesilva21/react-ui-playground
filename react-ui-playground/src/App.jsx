374734939ed6fc3c3e8fc60cae0740af2b3b8325
Yohani S. De Silva <yohanidesilva.dev.21@gmail.com>
2026-04-29 21:48:04 +0530

    add counter app ui

--- react-ui-playground/src/App.jsx
+++ react-ui-playground/src/App.jsx
@@ -1,7 +1,12 @@
+import Counter from './components/counter/counter'
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <>
+      <h1>React UI Collection</h1>
+      <Counter />
+    </>
+      
   )
 }
 
---
+++ react-ui-playground/src/components/counter/counter.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import './counter.css'
+
+function Counter(){
+    
+    const[count,setCount] = useState(0)
+
+    const incrementToggler = () =>{
+        setCount(count+1)
+    }
+    const decrementToggler = () =>{
+        setCount(count-1)
+    }
+    const resetToggler = () =>{
+        setCount(0)
+    }
+    return(
+        <div className="card flex counter-container">
+            <h2>Counter UI</h2>
+            <p>{count}</p>
+            <div className="counter-btns">
+                <button onClick={incrementToggler}>Increment</button>
+                <button className="danger-btn"onClick={resetToggler}>Reset</button>
+                <button onClick={decrementToggler}>Decrement</button>
+            </div>
+            
+        </div>
+    );
+}
+export default Counter
